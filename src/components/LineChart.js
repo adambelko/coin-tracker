03f3e79520d7dc7cb4d71f5e6ee522368241aa16
Adzi <belkoadam@gmail.com>
2023-07-11 14:53:30 +1000

    Update chart data with changed time intervals in place. Active interval state in place also

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -32,35 +32,38 @@ const TimeIntervalBar = styled.div`
   ul {
     display: flex;
     list-style: none;
-    padding: 3px;
+    padding: 4px;
+    cursor: pointer;
 
     li {
       padding: 0.4em 0.6em;
       white-space: nowrap;
+      border-radius: 6px;
+      &:hover {
+        background-color: #f8fafd;
+      }
+      &.active {
+        background-color: #ffffff;
+      }
     }
   }
 `;
 
 const LineChart = ({ coin }) => {
   const [chartData, setChartData] = useState();
+  const [timeInterval, setTimeInterval] = useState(30);
+  const [activeInterval, setActiveInterval] = useState(30);
   const coinTimeStamp = [];
   const coinPrice = [];
 
-  const chartDataAPI = `https://api.coingecko.com/api/v3/coins/${coin.id}/market_chart?vs_currency=usd&days=30&interval=daily&precision=full`;
+  const chartDataAPI = `https://api.coingecko.com/api/v3/coins/${coin.id}/market_chart?vs_currency=usd&days=${timeInterval}&interval=daily&precision=full`;
 
   useEffect(() => {
     axios
       .get(chartDataAPI)
       .then((response) => setChartData(response.data))
       .catch((error) => console.log(error));
-  }, []);
-
-  chartData?.prices?.forEach((data) => {
-    coinTimeStamp.push(new Date(data[0]).toLocaleDateString());
-    coinPrice.push(data[1].toFixed(1));
-  });
-
-  console.log(chartData);
+  }, [timeInterval]);
 
   const data = {
     labels: coinTimeStamp,
@@ -69,10 +72,20 @@ const LineChart = ({ coin }) => {
         label: "Price In USD",
         data: coinPrice,
         fill: false,
-        backgroundColor: "#0071bd",
-        borderColor: "#0071bd",
+        backgroundColor: "#4789f7",
+        borderColor: "#4789f7",
       },
     ],
+  };
+
+  chartData?.prices?.forEach((data) => {
+    coinTimeStamp.push(new Date(data[0]).toLocaleDateString());
+    coinPrice.push(data[1].toFixed(1));
+  });
+
+  const handleTimeIntervalClick = (interval) => {
+    setTimeInterval(interval);
+    setActiveInterval(interval);
   };
 
   return (
@@ -81,11 +94,36 @@ const LineChart = ({ coin }) => {
         <ChartContentBar></ChartContentBar>
         <TimeIntervalBar>
           <ul>
-            <li>1D</li>
-            <li>7D</li>
-            <li>1M</li>
-            <li>1Y</li>
-            <li>ALL</li>
+            <li
+              onClick={() => handleTimeIntervalClick(1)}
+              className={activeInterval === 1 ? "active" : ""}
+            >
+              1D
+            </li>
+            <li
+              onClick={() => handleTimeIntervalClick(7)}
+              className={activeInterval === 7 ? "active" : ""}
+            >
+              7D
+            </li>
+            <li
+              onClick={() => handleTimeIntervalClick(30)}
+              className={activeInterval === 30 ? "active" : ""}
+            >
+              1M
+            </li>
+            <li
+              onClick={() => handleTimeIntervalClick(365)}
+              className={activeInterval === 365 ? "active" : ""}
+            >
+              1Y
+            </li>
+            <li
+              onClick={() => handleTimeIntervalClick("max")}
+              className={activeInterval === "max" ? "active" : ""}
+            >
+              ALL
+            </li>
           </ul>
         </TimeIntervalBar>
       </ChartManager>
